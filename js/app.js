1ca88db3d8b0d53ed51fd2dcc512a5ea94e0237f
Jiovani Vianey <138537620+JiovaniVianey@users.noreply.github.com>
2023-09-28 16:31:50 +0200

    Rendu de l'exo + Mega Bonus de la mort qui fait peur

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const advertizer = {
     /**
      * Propriétés
      */
+    keylist: null,
 
     // Propriété permettant de modéliser le fait que la touche MAJ a été enfoncée
     isUpperCase: false,
@@ -13,9 +14,79 @@ const advertizer = {
      */
 
     init: function() {
-
         // Code à exécuter au lancement de la page
+        advertizer.keyList = document.querySelectorAll(".key.letter");
+        advertizer.keyshift = document.querySelector(".maj");
+        advertizer.keyback = document.querySelector(".return");
+        advertizer.keyspace = document.querySelector(".key.space");
+        advertizer.customColorInput = document.getElementById('custom-color-input');
 
+        for (const key of advertizer.keyList) {
+            key.addEventListener('click', advertizer.addLetter);
+        }
+
+        advertizer.keyshift.addEventListener('click', advertizer.toggleUpperCase);
+        advertizer.keyspace.addEventListener('click', advertizer.addSpace);
+        advertizer.keyback.addEventListener('click', advertizer.removeLetter);
+        advertizer.customColorInput.addEventListener('input', advertizer.changeTextColor);
+    },
+
+    addLetter: function(event) {
+        const screen = document.querySelector(".text");
+        let screentext = screen.textContent;
+        const btn = event.currentTarget;
+        let keyChar = btn.textContent;
+
+        if (advertizer.isUpperCase) {
+            keyChar = keyChar.toUpperCase();
+        }
+
+        screentext += keyChar;
+
+        screen.textContent = screentext;
+    },
+
+    removeLetter: function(event) {
+        const screen = document.querySelector(".text");
+        let screentext = screen.textContent;
+
+        screentext = screentext.slice(0,-1);
+
+        screen.textContent = screentext;
+    },
+
+    addSpace: function(event) {
+        const screen = document.querySelector(".text");
+        let screentext = screen.textContent;
+
+        screentext += " ";
+
+        screen.textContent = screentext;
+    },
+
+    toggleUpperCase() {
+        advertizer.isUpperCase = !advertizer.isUpperCase;
+
+        // Parcourez les boutons et ajoutez ou retirez la classe "pressed" en fonction de l'état des majuscules
+        for (const key of advertizer.keyList) {
+            if (advertizer.isUpperCase) {
+            key.classList.add('upper');
+            } else {
+            key.classList.remove('upper');
+            }
+        };
+
+        if (advertizer.isUpperCase) {
+            advertizer.keyshift.classList.add('pressed');
+        } else {
+            advertizer.keyshift.classList.remove('pressed');
+        }
+    },
+
+    changeTextColor: function() {
+        const colorValue = advertizer.customColorInput.value;
+        const screen = document.querySelector(".text");
+        screen.style.color = colorValue;
     }
 }
 
